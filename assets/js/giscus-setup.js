c0cc7d1bb86a104ab02b3bff5481fc6d86d31e53
julianppe <72886831+julianppe@users.noreply.github.com>
2026-01-02 14:47:21 +0000

    Deploying to gh-pages from @ julianppe/julianppe.github.io@6e12661b5ea3a950f4e064f47b573c7474c33f09 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "julianppe/julianppedrazzi.github.io",
+    "data-repo": "julianppe/julianppe.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
